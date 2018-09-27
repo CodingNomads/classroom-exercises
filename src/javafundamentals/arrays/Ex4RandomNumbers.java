0bc6872909277c8f037a683471293766a71f5bc5
Andy <andymitchell1@live.co.uk>
2018-09-27 11:16:59 -0500

    Random Numbers Submission

--- src/javafundamentals/arrays/Ex4RandomNumbers.java
+++ src/javafundamentals/arrays/Ex4RandomNumbers.java
@@ -5,6 +5,17 @@ package javafundamentals.arrays;
  */
 
 public class Ex4RandomNumbers {
+    public static void main(String[] args) {
+        int[] randomArray = {12,143,654,34,5,76,37,42};
 
+        int biggestNumberIndex = 0;
 
+        for (int i = 0; i < randomArray.length; i++) {
+            if(randomArray[i] > randomArray[biggestNumberIndex]) {
+                biggestNumberIndex = i;
+            }
+        }
+
+        System.out.println(biggestNumberIndex);
+    }
 }
