7dbfa8665f6ea82660a3d969b97533dc18f93732
Roi Otero Morales <roioteromorales@gmail.com>
2018-09-27 11:31:30 -0500

    Add common elements and reverse array

---
+++ src/javafundamentals/arrays/CommonElements.java
@@ -0,0 +1,10 @@
+package javafundamentals.arrays;
+
+/**
+ * Create an array with the first 20 multiples of 12 and the first 20 multiples of 10.
+ * Find the common elements between the 2 arrays and print them
+ * */
+
+public class CommonElements {
+
+}
---
+++ src/javafundamentals/arrays/ReverseArray.java
@@ -0,0 +1,8 @@
+package javafundamentals.arrays;
+
+/**
+ * Create an array with 10 values on it and print the values of the array in the reverse order
+ * */
+
+public class ReverseArray {
+}
