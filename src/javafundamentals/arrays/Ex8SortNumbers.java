b61309f5a60eb49e5a3fe2f1f522b9e181ab00e1
andreahowes <ashcreek.howes@gmailcom>
2018-09-27 17:53:14 -0500

    commit for arrays

--- src/javafundamentals/arrays/Ex8SortNumbers.java
+++ src/javafundamentals/arrays/Ex8SortNumbers.java
@@ -1,8 +1,33 @@
 package javafundamentals.arrays;
 
+import java.util.Arrays;
+
 /**
  * Create an array with 10 random numbers (between 0 and 1000) and create a method that sorts them.
  */
 
 public class Ex8SortNumbers {
+    public static void main(String[] args) {
+        int[] randomArray = new int[10];
+        sortArray(createArray(randomArray));
+    }
+
+    public static int[] createArray(int[] theArray){
+        for (int i = 0; i < 10; i++) {
+            theArray[i]= (int)(Math.random()*1000);
+            //System.out.println(theArray[i]);
+        }
+        return theArray;
+    }
+
+    public static void sortArray(int[] theArray){
+        Arrays.sort(theArray);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(theArray[i]);
+        }
+    }
+
+
+
+
 }
