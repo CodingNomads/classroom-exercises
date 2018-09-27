0d13258a8f75752c4c23a735d9f33c6f388506dd
Roi Otero Morales <roioteromorales@gmail.com>
2018-09-27 12:28:31 -0500

    Add missin comment for Sortnumbers

--- src/javafundamentals/arrays/Ex8SortNumbers.java
+++ src/javafundamentals/arrays/Ex8SortNumbers.java
@@ -1,4 +1,8 @@
 package javafundamentals.arrays;
 
+/**
+ * Create an array with 10 random numbers (between 0 and 1000) and create a method that sorts them.
+ * */
+
 public class Ex8SortNumbers {
 }
