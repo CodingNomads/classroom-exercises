8d423c484ace15ca8777eaa340e153df65d2b1b0
Roi Otero Morales <roioteromorales@gmail.com>
2018-09-27 11:21:56 -0500

    Add chess table

---
+++ src/javafundamentals/arrays/BidimensionalArray.java
@@ -0,0 +1,16 @@
+package javafundamentals.arrays;
+
+/**
+ * Create a bidimensional array containing the values of the chess table and print it
+ *   a  b  c  d  e  f  g  h
+ * 1 a1 b1 c1
+ * 2 a2 b2
+ * 3 a3 b3
+ * 4
+ * 5
+ * 6
+ * 7
+ * 8
+ * */
+public class BidimensionalArray {
+}
