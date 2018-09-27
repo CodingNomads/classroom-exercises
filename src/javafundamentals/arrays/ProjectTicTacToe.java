3ae9b44109129949f99a0a0ebaa392f421aad576
Roi Otero Morales <roioteromorales@gmail.com>
2018-09-27 12:43:04 -0500

    Add Ex8 and project tic tac toe

---
+++ src/javafundamentals/arrays/ProjectTicTacToe.java
@@ -0,0 +1,16 @@
+package javafundamentals.arrays;
+
+
+/**
+ * Fun time! Lets play tic tac toe! You will create your first videogame! Yeah!
+ *
+ * Create a tic tac toe game (https://www.google.com/search?q=tic+tac+toe).
+ *
+ * The player will play with 'X' and the IA will play with 'O'
+ * The player will start. The game will ask via console where to put the 'X'.
+ * After the IA will put in a random slot (not used yet) a 'O'
+ * The game will go on until there are no more spots left or one of the players will have 3 in a row
+ * */
+
+public class ProjectTicTacToe {
+}
