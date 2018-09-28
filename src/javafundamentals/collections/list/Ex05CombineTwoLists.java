3b6d86d94b35be926aca432b5e00ff6822ed7854
Andy <andymitchell1@live.co.uk>
2018-09-28 16:34:39 -0500

    Andy's collections submission

--- src/javafundamentals/collections/list/Ex05CombineTwoLists.java
+++ src/javafundamentals/collections/list/Ex05CombineTwoLists.java
@@ -1,5 +1,8 @@
 package javafundamentals.collections.list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Create a list containing 3 european city names.
  * Create another list containing 3 american city names.
@@ -7,4 +10,23 @@ package javafundamentals.collections.list;
  */
 
 public class Ex05CombineTwoLists {
+    public static void main(String[] args) {
+        List<String> europeanCities = new ArrayList<>();
+
+        europeanCities.add("Portsmouth");
+        europeanCities.add("Vigo");
+        europeanCities.add("Krakow");
+
+        List<String> usCities = new ArrayList<>();
+        usCities.add("New York City");
+        usCities.add("Dallas");
+        usCities.add("Denver");
+
+        List<String> globalCities = new ArrayList<>();
+
+        globalCities.addAll(europeanCities);
+        globalCities.addAll(usCities);
+
+        System.out.println(globalCities);
+    }
 }
--- src/javafundamentals/collections/list/Ex06ClearingTheList.java
+++ src/javafundamentals/collections/list/Ex06ClearingTheList.java
@@ -1,9 +1,24 @@
 package javafundamentals.collections.list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Create a list with random numbers from 1 to 5
  * If the list contains a 2, delete all the elements in the list resulting into an empty list.
  */
 
 public class Ex06ClearingTheList {
+    public static void main(String[] args) {
+        List<Integer> randomNoList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            randomNoList.add((int)(Math.random() * 5) +1);
+        }
+        System.out.println(randomNoList);
+        if (randomNoList.contains(2)) {
+            randomNoList.clear();
+        }
+        System.out.println(randomNoList);
+    }
 }
