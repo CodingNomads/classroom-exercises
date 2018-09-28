61afa5673d4d9fca2e4e5b1ecf8480bbf5ae5936
Andy <andymitchell1@live.co.uk>
2018-09-28 16:34:39 -0500

    Andy's collections submission

--- src/javafundamentals/collections/list/Ex02Iterate.java
+++ src/javafundamentals/collections/list/Ex02Iterate.java
@@ -1,9 +1,27 @@
 package javafundamentals.collections.list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Create a List with numbers from 1 to 10.
  * Print the sum of all the values in the list.
  */
 
 public class Ex02Iterate {
+    public static void main(String[] args) {
+        List<Integer> integerList = new ArrayList<>();
+
+        for (int i = 0; i <= 10; i++) {
+            integerList.add(i);
+        }
+
+        int total = 0;
+        for (int i:
+             integerList) {
+            total += i;
+        }
+
+        System.out.println(total);
+    }
 }
--- src/javafundamentals/collections/list/Ex04DeleteElements.java
+++ src/javafundamentals/collections/list/Ex04DeleteElements.java
@@ -1,9 +1,24 @@
 package javafundamentals.collections.list;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Create an List with numbers from 1 to 10
  * Delete the number 7 and print the list
  */
 
 public class Ex04DeleteElements {
+    public static void main(String[] args) {
+        List<Integer> numberList = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+            numberList.add(i);
+        }
+
+        numberList.remove(numberList.indexOf(7));
+
+        System.out.println(numberList);
+    }
 }
