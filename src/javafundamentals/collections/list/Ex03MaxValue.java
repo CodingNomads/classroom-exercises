ac42b2b45c34bb84d5ffd19e54a6a01e1c441961
Andy <andymitchell1@live.co.uk>
2018-09-28 16:34:39 -0500

    Andy's collections submission

--- src/javafundamentals/collections/list/Ex03MaxValue.java
+++ src/javafundamentals/collections/list/Ex03MaxValue.java
@@ -1,9 +1,24 @@
 package javafundamentals.collections.list;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Create a List with 20 random numbers.
  * Find the maximum value of the list and print it.
  */
 
 public class Ex03MaxValue {
+    public static void main(String[] args) {
+        List<Integer> randomNoList = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+            randomNoList.add((int)(Math.random() * 100 -1) +1);
+        }
+
+        System.out.println(randomNoList);
+        System.out.println(Collections.max(randomNoList) + " is the biggest");
+
+    }
 }
