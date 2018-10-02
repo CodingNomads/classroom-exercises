38564699b64fc4a33a477dad2ec5550be83f3540
Michael Ramey <mike.ramey@gmail.com>
2018-10-02 09:34:06 -0500

    arrays exercise_03 complete

--- src/javafundamentals/arrays/Ex3AgeStatistics.java
+++ src/javafundamentals/arrays/Ex3AgeStatistics.java
@@ -8,7 +8,36 @@ package javafundamentals.arrays;
 public class Ex3AgeStatistics {
     public static void main(String[] args) {
 
+        int[] ageArray = new int[5];
+        ageArray[0] = 7;
+        ageArray[1] = 12;
+        ageArray[2] = 20;
+        ageArray[3] = 25;
+        ageArray[4] = 3;
+
+        int avgAge = 0;
+        int sum = 0;
+        int youngest = 1000;
+        int oldest = -1;
+
+        for (int i = 0; i < ageArray.length; i++) {
+            sum += ageArray[i];
+            if (ageArray[i] < youngest) {
+                youngest = ageArray[i];
+            }
+            if (ageArray[i] > oldest) {
+                oldest = ageArray[i];
+            }
+        }
+
+        avgAge = sum / ageArray.length;
+        System.out.println(sum);
+        System.out.println(avgAge);
+        System.out.println(youngest);
+        System.out.println(oldest);
 
 
     }
+
+
 }
