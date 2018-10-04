9e11dad8cdcef34d3efa383d9adaf9084e795422
Roi Otero Morales <roioteromorales@gmail.com>
2018-10-04 16:50:05 -0500

    Add lambda exercise

---
+++ src/javafundamentals/lambdas/Lambdas.java
@@ -0,0 +1,19 @@
+package javafundamentals.lambdas;
+
+/**
+ * Create a class with 2 methods.
+ * One accepts a Predicate and another that accepts a function
+ *
+ * Use anonymous classes to pass to those methods
+ * Replace them with lambdas
+ */
+
+public class Lambdas {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
