42cd592e8b4fd65430b76d4ac3c600f2a40057da
Roi Otero Morales <roioteromorales@gmail.com>
2018-10-09 10:45:27 -0500

    Add streams exercises

---
+++ src/javafundamentals/streams/Ex01Stream.java
@@ -0,0 +1,9 @@
+package javafundamentals.streams;
+
+/**
+ * Create an stream of strings with the names of the people in the class
+ * Iterate it and print the values.
+ */
+
+public class Ex01Stream {
+}
