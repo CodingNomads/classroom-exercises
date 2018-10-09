87bc92633c0d87604ddb3a5096a4de9d8b71ac66
Andy <andymitchell1@live.co.uk>
2018-10-09 09:29:37 -0500

    Annotation and enum submissions

---
+++ src/javafundamentals/enums/Months.java
@@ -0,0 +1,16 @@
+package javafundamentals.enums;
+
+public enum Months {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+}
