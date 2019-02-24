55ba463542562d83a6ecc06af7bc3a4d690f89cc
Cristian Perez <cristian.perez2011@gmail.com>
2019-02-24 18:07:55 +0800

    Finish Interfaces Package

--- src/main/java/interfaces/Ex01Interfaces.java
+++ src/main/java/interfaces/Ex01Interfaces.java
@@ -8,4 +8,20 @@ package interfaces;
  * Put all the created classes into an array and iterate calling a method from the interface.
  */
 public class Ex01Interfaces {
+    public static void main(String[] args) {
+
+        Electronics smartwatch = new Electronics();
+        Smartphone iPhone = new Smartphone();
+        Computer macBook = new Computer();
+
+        Electronics[] electroTown = {smartwatch,iPhone,macBook};
+
+        for (Electronics i : electroTown) {
+            i.turnOn();
+            i.takeInput();
+            i.consumeElectricity();
+            System.out.println();
+        }
+
+    }
 }
