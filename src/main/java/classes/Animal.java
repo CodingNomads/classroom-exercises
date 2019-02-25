a0f2255e0ae4be47ee1ec730eb53a161f5d05911
Eileen Ai <eileen.aix@gmail.com>
2019-02-25 09:21:19 +0800

    wip

---
+++ src/main/java/classes/Animal.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class Animal {
+
+    private String animal;
+
+    //the method for entering cage
+    public void enterCage(String animal){
+        this.animal = animal;
+        System.out.println(animal + " has entered cage!");
+    }
+
+
+}
