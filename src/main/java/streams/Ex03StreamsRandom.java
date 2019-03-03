0c84c81c0cb22d54242d10e77ae70fae6e683de5
alpdurmaz <alp_durmaz@hotmail.com>
2019-03-03 14:01:51 +0800

    alp_streams

--- src/main/java/streams/Ex03StreamsRandom.java
+++ src/main/java/streams/Ex03StreamsRandom.java
@@ -1,10 +1,27 @@
 package streams;
 
+import java.util.List;
+import java.util.stream.*;
+import java.util.Random;
+
 /**
- * Create an stream oof 100 elements with random values on them.
+ * Create an stream of 100 elements with random values on them.
  * filter the odd ones out and pick only 3 results
  * put them into a List.
  */
 
 public class Ex03StreamsRandom {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        List<Integer> numbers = Stream
+                .generate(()-> random.nextInt())
+                .limit(100)
+                .filter(val-> val % 2 != 0)
+                .limit(3)
+                .collect(Collectors.toList());
+
+        System.out.println(numbers);
+    }
 }
