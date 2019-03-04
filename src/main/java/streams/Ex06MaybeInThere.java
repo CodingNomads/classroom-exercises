a42f029f223f87fd443489aa20f7ac69a7ec794d
Jordan <jshenolikar@gmail.com>
2019-03-04 10:07:50 +0800

    streams ex06

--- src/main/java/streams/Ex06MaybeInThere.java
+++ src/main/java/streams/Ex06MaybeInThere.java
@@ -1,5 +1,11 @@
 package streams;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * Create a list with 10 random elements.
  * Find any element
@@ -8,4 +14,17 @@ package streams;
  */
 
 public class Ex06MaybeInThere {
+
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10000; i++) {
+
+            IntStream randomstream = new Random().ints(10);
+            List<Integer> randomlist = randomstream.boxed().collect(Collectors.toList());
+            Optional<Integer> test = randomlist.stream().filter(x -> x > 1000000000).findAny();
+            System.out.println(test.orElse(0));
+
+        }
+    }
 }
